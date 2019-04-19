0b0a2446e2e91ed82221890ddf75b1d4208929f4
Mihai Preo <mihaipreo@gmail.com>
2019-04-19 16:25:14 +0100

    Events added. Tests added.

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import "./App.css";
 import "./bootstrap.min.css";
 
@@ -13,28 +14,57 @@ function Hero() {
   );
 }
 
-function Book({ title }) {
+function Book({ title, onClick }) {
   return (
-    <div className="answer">
+    <div
+      className="answer"
+      onClick={() => {
+        onClick(title);
+      }}
+    >
       <h4>{title}</h4>
     </div>
   );
 }
 
-function Turn({ author, books }) {
+function Turn({ author, books, highlight, onAnswerSelected }) {
+  function highlightToBgColor(highlight) {
+    const mapping = {
+      none: "",
+      correct: "green",
+      wrong: "red"
+    };
+    return mapping[highlight];
+  }
+
   return (
-    <div className="row turn" style={{ backgroundColor: "white" }}>
+    <div
+      className="row turn"
+      style={{ backgroundColor: highlightToBgColor(highlight) }}
+    >
       <div className="col-4 offset-1">
         <img src={author.imageUrl} className="authorimage" alt="Author" />
       </div>
       <div>
         {books.map(title => (
-          <Book title={title} key={title} />
+          <Book title={title} key={title} onClick={onAnswerSelected} />
         ))}
       </div>
     </div>
   );
 }
+
+Turn.propTypes = {
+  author: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    imageUrl: PropTypes.string.isRequired,
+    imageSource: PropTypes.string.isRequired,
+    books: PropTypes.arrayOf(PropTypes.string).isRequired
+  }),
+  books: PropTypes.arrayOf(PropTypes.string).isRequired,
+  highlight: PropTypes.string.isRequired,
+  onAnswerSelected: PropTypes.func.isRequired
+};
 
 function Continue() {
   return <div> </div>;
@@ -56,11 +86,15 @@ function Footer() {
   );
 }
 
-function AuthorQuiz({ turnData }) {
+function AuthorQuiz({ turnData, highlight, onAnswerSelected }) {
   return (
     <div className="container-fluid">
       <Hero />
-      <Turn {...turnData} />
+      <Turn
+        {...turnData}
+        highlight={highlight}
+        onAnswerSelected={onAnswerSelected}
+      />
       <Continue />
       <Footer />
     </div>
